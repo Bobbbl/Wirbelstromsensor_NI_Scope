6bc95b29c3926e2aeb0ade5f07135cf9d3f7c540
Sebastian Draxinger <sebastian.draxinger@googlemail.com>
2016-03-11 14:21:21 +0100

    Interface hinzugefügt für Push-Liste

---
+++ Wirbelstromsensor_NI_Scope/IPushNotificableList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wirbelstromsensor_NI_Scope
+{
+    interface IPushNotificableList
+    {
+        /// <summary>
+        /// Fügt den aktuellen wert vorne an die Liste an und schiebt
+        /// den Rest nach hinten.
+        /// </summary>
+        void Push();
+    }
+}
